17d7e5e2e8634ace188aed05e2307c85f35f1f01
clement brossette <clement.brossette@ynov.com>
2016-03-15 09:25:31 +0100

    packages

---
+++ AppliJava/ProjetTransversJava/src/com/clement/seydina/Object/Json.java
@@ -0,0 +1,99 @@
+package com.clement.seydina.Object;
+import java.util.ArrayList;
+
+import javax.swing.JLabel;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Json {
+
+	private JSONArray search ;
+	private String account;
+	
+	public JSONArray getSearch() {
+		return search;
+	}
+	public void setSearch(JSONArray jArray) {
+		this.search = jArray;
+	}
+
+	public Json(JSONArray apiReturn){
+		this.search = apiReturn;
+	}
+	public Json(String str){
+		this.account = str;
+	}
+	
+	public ArrayList<Tweet> ParseJson() throws JSONException {
+		
+		
+		ArrayList<Tweet> listTweet = new ArrayList<Tweet>();
+		try{	
+		    
+		    
+		    //put the value we need in tweets 
+		    for (int i=0;i<search.length();i++){
+		    	JSONObject jsonObject = search.getJSONObject(i);
+		    	String createdAt = jsonObject.getString("created_at");
+		    	String text = jsonObject.getString("text");
+		    	
+		    	JSONObject jsonObject2 = jsonObject.getJSONObject("user");
+		    	String name = jsonObject2.getString("name");
+		    	String screenName = jsonObject2.getString("screen_name");
+		    	String avatar = jsonObject2.getString("profile_image_url");
+		    	
+		    	listTweet.add(new Tweet(createdAt,text,name,screenName,avatar));
+		    	
+		    }
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return listTweet;
+	}
+	
+	public ArrayList<Tweet> ParseJsonSearch() throws JSONException {
+		
+		ArrayList<Tweet> listTweet = new ArrayList<Tweet>();
+		try{	
+		    
+		    
+		    //put the value we need in tweets 
+		    for (int i=0;i<search.length();i++){
+		    	JSONObject jsonObject = search.getJSONObject(i);
+		    	String createdAt = jsonObject.getString("created_at");
+		    	String text = jsonObject.getString("text");
+		    	
+		    	JSONObject jsonObject2 = jsonObject.getJSONObject("user");
+		    	String name = jsonObject2.getString("name");
+		    	String screenName = jsonObject2.getString("screen_name");
+		    	String avatar = jsonObject2.getString("profile_image_url");
+		    	
+		    	listTweet.add(new Tweet(createdAt,text,name,screenName,avatar));
+		    	
+		    }
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return listTweet;
+	}
+	
+	public JLabel ParseJsonAccount() throws JSONException{
+		
+		JSONObject jsonObject = new JSONObject(account);
+		JLabel account = null;
+		try{
+			String name = jsonObject.getString("name");
+			String screen_name = jsonObject.getString("screen_name");
+			String profile_image_url = jsonObject.getString("profile_image_url");
+			
+			account = new JLabel("<html>"+"<img src="+profile_image_url+">"
+					 +"<b>"+name +"</b> @" + screen_name);
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return account;
+	}
+}
