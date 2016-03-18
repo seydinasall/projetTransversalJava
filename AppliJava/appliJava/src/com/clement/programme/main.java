7d98959a97cb7672e6727ed754883e12a1a9d185
clement brossette <clement.brossette@ynov.com>
2016-03-18 14:50:43 +0100

    fini

---
+++ AppliJava/appliJava/src/com/clement/programme/main.java
@@ -0,0 +1,18 @@
+package com.clement.programme;
+
+import com.clement.gui.MainWindow;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		try{
+		MainWindow window = new MainWindow();  
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+}
